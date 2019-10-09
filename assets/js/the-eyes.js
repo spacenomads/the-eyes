6f53096cb71deee3597d3061750a2255eedbbc25
Carlos Mañas <carlos@spacenomads.com>
2019-10-09 20:57:11 +0200

    Make the eye follow the user cursor movements

--- assets/js/the-eyes.js
+++ assets/js/the-eyes.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('yay');
+var balls = document.querySelectorAll('.js__eye');
+
+document.onmousemove = function() {
+	var x = event.clientX * 100 / window.innerWidth + '%';
+	var y = event.clientY * 100 / window.innerHeight + '%';
+
+	for (var i=0;i<balls.length;i++) {
+		balls[i].style.left = x;
+		balls[i].style.top = y;
+		balls[i].style.transform = `translate(-${x}, -${y})`;
+	}
+};
