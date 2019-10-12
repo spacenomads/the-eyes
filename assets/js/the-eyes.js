6f08fbe81b1f444ddde75be888480b41df138a32
Carlos Mañas <carlos@spacenomads.com>
2019-10-12 15:04:33 +0200

    Fix axys position when the eye is not centered

--- assets/js/the-eyes.js
+++ assets/js/the-eyes.js
@@ -4,9 +4,29 @@ const eyeball = document.querySelector('.js__eyeball');
 const timerMS = 1200;
 
 
+function getXtoCenter() {
+	const eyeBox = eye.getBoundingClientRect();
+	const eyeXCenter = eyeBox.x + eyeBox.width / 2;
+	const windowXCenter = window.innerWidth / 2;
+	const xOffset = windowXCenter - eyeXCenter;
+	return xOffset * 100 / window.innerWidth;
+}
+
+function getYtoCenter() {
+	const eyeBox = eye.getBoundingClientRect();
+	const eyeYCenter = eyeBox.y + eyeBox.height / 2;
+	const windowYCenter = window.innerHeight / 2;
+	const yOffset = windowYCenter - eyeYCenter;
+	return yOffset * 100 / window.innerHeight;
+}
+
 function lookAtTheCursor() {
-	const x = event.clientX * 100 / window.innerWidth + '%';
-	const y = event.clientY * 100 / window.innerHeight + '%';
+	const xFix = getXtoCenter();
+	const yFix = getYtoCenter();
+
+	const x = (event.clientX * 100 / window.innerWidth) + xFix + '%';
+	const y = (event.clientY * 100 / window.innerHeight) + yFix + '%';
+
 
 	eyeball.style.left = x;
 	eyeball.style.top = y;
